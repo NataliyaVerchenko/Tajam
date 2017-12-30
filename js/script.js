ff6656dcaca9c2d272e55b819fd1b07addb60d17
NataliaVerchenko <ver4.nata@gmail.com>
2017-12-30 02:58:00 +0000

    create block tesmonial slider

--- js/script.js
+++ js/script.js
@@ -11,6 +11,42 @@ $(document).ready(function () {
 
 
     });
+    $('.slider-testimonial').slick({
+        adaptiveHeight: true,
+        dots: false,
+        arrows:true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '5px'
+
+
+    });
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
 
 });
